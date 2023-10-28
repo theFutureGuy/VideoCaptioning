6eea69dfab63aa9ea0bc5c6f50d8a14a52178c7f
theFutureGuy <ashiqgit789@gmail.com>
2023-10-28 13:31:45 +0530

    The video caption merge test 2 half success

--- VideoSubtitleProcessor.py
+++ VideoSubtitleProcessor.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
+import shutil
 import speech_recognition as sr
 from googletrans import Translator
-import shutil
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 class VideoSubtitleProcessor:
-    def __init__(self):
-        self.target_language = 'es' 
+    def __init__(self, target_language='es'):
+        self.target_language = target_language
 
     def set_target_language(self, target_language):
         self.target_language = target_language
@@ -15,37 +17,35 @@ class VideoSubtitleProcessor:
         if target_language:
             self.set_target_language(target_language)
 
-        if not output_dir:
-            output_dir = os.path.dirname(input_video)
+        if output_dir is None:
+            output_dir = os.path.join(os.path.dirname(input_video), "output_videos")
+
+        os.makedirs(output_dir, exist_ok=True)  # Create the output directory if it doesn't exist
 
         base_name = os.path.splitext(os.path.basename(input_video))[0]
         audio_path = os.path.join(output_dir, f'{base_name}.wav')
         recognized_text_path = os.path.join(output_dir, f'{base_name}_recognized.txt')
         translated_text_path = os.path.join(output_dir, f'{base_name}_translated.txt')
-        srt_path = os.path.join(output_dir, f'{base_name}.srt')
-        output_video_path = os.path.join(output_dir, f'{base_name}_output.mp4')
+        srt_dir = os.path.join(output_dir, 'srt_frames')
+        os.makedirs(srt_dir, exist_ok=True)  # Create the SRT frames directory
 
         self.extract_audio(input_video, audio_path)
-        recognized_text = self.recognize_speech(audio_path)
-        translated_text = self.translate_text(recognized_text)
+        self.recognize_speech(audio_path, recognized_text_path)
+        self.translate_text(recognized_text_path, translated_text_path)
 
-        with open(recognized_text_path, 'w', encoding='utf-8') as recognized_file:
-            recognized_file.write(recognized_text)
+        # Generate SRT subtitles for each frame
+        self.generate_srt_frames(input_video, srt_dir)
 
-        with open(translated_text_path, 'w', encoding='utf-8') as translated_file:
-            translated_file.write(translated_text)
-
-        with open(srt_path, 'w', encoding='utf-8') as srt_file:
-            srt_file.write("1\n00:00:00,000 --> 00:00:05,000\n" + translated_text_path)
-
-        self.merge_srt_with_video(input_video, srt_path, output_video_path)
+        # Merge SRT frames with the video
+        output_video = os.path.join(output_dir, f'{base_name}_output.mp4')
+        self.merge_srt_with_video(input_video, srt_dir, output_video)
 
         os.remove(audio_path)
         os.remove(recognized_text_path)
         os.remove(translated_text_path)
-        os.remove(srt_path)
+        shutil.rmtree(srt_dir) 
 
-        return output_video_path
+        return output_video
 
     def extract_audio(self, input_video, output_audio):
         command = [
@@ -59,27 +59,89 @@ class VideoSubtitleProcessor:
         ]
         subprocess.run(command)
 
-    def recognize_speech(self, audio_path):
+    def recognize_speech(self, audio_path, recognized_text_path):
         r = sr.Recognizer()
         with sr.AudioFile(audio_path) as source:
             audio = r.record(source)
 
-        return r.recognize_google(audio, language='en-US')
+        recognized_text = r.recognize_google(audio, language='en-US')
 
-    def translate_text(self, text):
+        with open(recognized_text_path, 'w', encoding='utf-8') as text_file:
+            text_file.write(recognized_text)
+
+    def translate_text(self, text_path, translated_text_path):
         if self.target_language == 'en':
-            # No need to translate if the target language is English
-            return text
+            shutil.copy(text_path, translated_text_path)
         else:
             translator = Translator()
-            translated = translator.translate(text, dest=self.target_language)
-            return translated.text
+            with open(text_path, 'r', encoding='utf-8') as text_file:
+                text = text_file.read()
 
-    def merge_srt_with_video(self, input_video, srt_path, output_video_path):
-        command = [
-            'ffmpeg',
-            '-i', input_video,
-            '-vf', f'subtitles={srt_path}',
-            output_video_path
-        ]
-        subprocess.run(command)
+            translated = translator.translate(text, dest=self.target_language)
+            translated_text = translated.text
+
+            with open(translated_text_path, 'w', encoding='utf-8') as translated_file:
+                translated_file.write(translated_text)
+
+    def generate_srt_frames(self, input_video, srt_dir):
+        clip = VideoFileClip(input_video)
+        frame_rate = clip.fps
+
+        translated_text_path = f'{os.path.splitext(input_video)[0]}_translated.txt'
+        with open(translated_text_path, 'r', encoding='utf-8') as text_file:
+            translated_text = text_file.read().splitlines()
+
+        frame_duration = 1 / frame_rate
+
+        for i, text in enumerate(translated_text):
+            start_time = i * frame_duration
+            end_time = (i + 1) * frame_duration
+            srt_filename = os.path.join(srt_dir, f'{i:04d}.srt')
+
+            with open(srt_filename, 'w', encoding='utf-8') as srt_file:
+                srt_file.write(f"{i + 1}\n{self.format_time(start_time)} --> {self.format_time(end_time)}\n{text}")
+
+    def merge_srt_with_video(self, input_video, srt_dir, output_video):
+        temp_audio_file = 'temp_audio.wav'
+        ffmpeg_extract_subclip(input_video, 0, 1, targetname=temp_audio_file)
+        video_clip = VideoFileClip(input_video)
+        audio_clip = VideoFileClip(temp_audio_file)
+        audio_duration = audio_clip.duration
+
+        clips = []
+
+        for srt_file in sorted(os.listdir(srt_dir)):
+            srt_path = os.path.join(srt_dir, srt_file)
+            start_time, end_time, text = self.parse_srt(srt_path)
+
+            if start_time < audio_duration:
+                sub_audio_clip = audio_clip.subclip(start_time, end_time)
+                sub_audio_clip = sub_audio_clip.volumex(0)
+                sub_video_clip = TextClip(text, fontsize=24, color='white', bg_color='black')
+                sub_video_clip = sub_video_clip.set_audio(sub_audio_clip)
+                clips.append(sub_video_clip)
+
+        final_clip = CompositeVideoClip(clips)
+        final_clip = final_clip.set_audio(audio_clip)
+
+        final_clip.write_videofile(output_video, codec="libx264", audio_codec="aac")
+
+        os.remove(temp_audio_file)
+
+    def format_time(self, time):
+        hours, remainder = divmod(time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f'{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d},000'
+
+    def parse_srt(self, srt_path):
+        with open(srt_path, 'r', encoding='utf-8') as srt_file:
+            lines = srt_file.readlines()
+            start_time, end_time = map(lambda x: x.strip(), lines[1].split("-->"))
+            text = lines[2].strip()
+        return start_time, end_time, text
+
+if __name__ == "__main__":
+    processor = VideoSubtitleProcessor(target_language='hi')  
+    input_video = 'path/to/your/input_video.mp4'  
+    output_video = processor.process_video(input_video)
+    print(f"Processed video saved to: {output_video}")
--- app.py
+++ app.py
@@ -1,34 +1,143 @@
-from flask import Flask, render_template, request, redirect
-from VideoSubtitleProcessor import VideoSubtitleProcessor
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import os
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from googletrans import Translator
 
-app = Flask(__name__, template_folder='templates')
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['OUTPUT_FOLDER'] = 'output_videos'
+app = Flask(__name__)
 
-processor = VideoSubtitleProcessor()
+UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'output_videos'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+class VideoSubtitleProcessor:
+    def __init__(self, input_video, target_language='es'):
+        self.input_video = input_video
+        self.target_language = target_language
+        self.output_dir = os.path.join(OUTPUT_FOLDER, os.path.splitext(os.path.basename(input_video))[0])
+
+    def process_video(self):
+        # Extract audio from the video
+        audio_path = os.path.join(self.output_dir, 'audio.wav')
+        self.extract_audio(audio_path)
+
+        # Recognize speech from the audio
+        recognized_text_path = os.path.join(self.output_dir, 'recognized.txt')
+        self.recognize_speech(audio_path, recognized_text_path)
+
+        # Translate the recognized text to the target language
+        translated_text_path = os.path.join(self.output_dir, f'translated_{self.target_language}.txt')
+        self.translate_text(recognized_text_path, translated_text_path)
+
+        # Generate SRT subtitles
+        srt_filename = os.path.join(self.output_dir, f'subtitles_{self.target_language}.srt')
+        self.generate_srt(translated_text_path, srt_filename)
+
+        # Merge SRT subtitles with the video
+        output_video = os.path.join(self.output_dir, 'output.mp4')
+        self.merge_srt_with_video(output_video, srt_filename)
+        return output_video
+
+    def extract_audio(self, output_audio):
+        video_clip = VideoFileClip(self.input_video)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(output_audio)
+
+    def recognize_speech(self, audio_path, recognized_text_path):
+        # Implement speech recognition here or use your preferred method
+        # Replace the following lines with your recognition code
+        recognized_text = "Recognized text from audio\nLine 2\nLine 3"
+        with open(recognized_text_path, 'w', encoding='utf-8') as text_file:
+            text_file.write(recognized_text)
+
+    def translate_text(self, text_path, translated_text_path):
+        translator = Translator()
+        with open(text_path, 'r', encoding='utf-8') as text_file:
+            text = text_file.read()
+
+        translated = translator.translate(text, dest=self.target_language)
+        translated_text = translated.text
+
+        with open(translated_text_path, 'w', encoding='utf-8') as translated_file:
+            translated_file.write(translated_text)
+
+    def generate_srt(self, translated_text_path, srt_filename):
+        with open(translated_text_path, 'r', encoding='utf-8') as text_file:
+            lines = text_file.readlines()
+            frame_duration = 2.0  # Adjust as needed
+
+            with open(srt_filename, 'w', encoding='utf-8') as srt_file:
+                for i, line in enumerate(lines):
+                    start_time = i * frame_duration
+                    end_time = (i + 1) * frame_duration
+                    srt_file.write(f"{i+1}\n{self.format_time(start_time)} --> {self.format_time(end_time)}\n{line}\n\n")
+
+    def merge_srt_with_video(self, output_video, srt_filename):
+        temp_audio_file = 'temp_audio.wav'
+        ffmpeg_extract_subclip(self.input_video, 0, 1, targetname=temp_audio_file)
+        video_clip = VideoFileClip(self.input_video)
+        audio_clip = VideoFileClip(temp_audio_file)
+        audio_duration = audio_clip.duration
+
+        clips = []
+
+        with open(srt_filename, 'r', encoding='utf-8') as srt_file:
+            lines = srt_file.readlines()
+            for i in range(0, len(lines), 4):
+                start_time, end_time, text = lines[i + 2].strip(), lines[i + 3].strip(), lines[i + 1].strip()
+                start_time = self.parse_time(start_time)
+                end_time = self.parse_time(end_time)
+                if start_time < audio_duration:
+                    sub_audio_clip = audio_clip.subclip(start_time, end_time)
+                    sub_audio_clip = sub_audio_clip.volumex(0)
+                    sub_video_clip = TextClip(text, fontsize=24, color='white', bg_color='black')
+                    sub_video_clip = sub_video_clip.set_audio(sub_audio_clip)
+                    clips.append(sub_video_clip)
+
+        final_clip = CompositeVideoClip(clips)
+        final_clip = final_clip.set_audio(audio_clip)
+
+        final_clip.write_videofile(output_video, codec="libx264", audio_codec="aac")
+
+        os.remove(temp_audio_file)
+
+    def parse_time(self, time_str):
+        hours, minutes, seconds = map(float, time_str.split(':'))
+        return hours * 3600 + minutes * 60 + seconds
+
+    def format_time(self, time):
+        hours, remainder = divmod(time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d},000"
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', processing_done=False, video_path=None)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
 
-@app.route('/process_video', methods=['POST'])
-def process_video():
-    if 'file' not in request.files:
-        return redirect(request.url)
+        file = request.files['file']
 
-    file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
 
-    if file.filename == '':
-        return redirect(request.url)
+        if file:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            target_language = request.form['target_language']
+            processor = VideoSubtitleProcessor(filename, target_language)
+            output_video = processor.process_video()
+            return redirect(url_for('download', filename=output_video))
 
-    if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
-        target_language = request.form.get('target_language')
-        output_video_path = processor.process_video(filename, target_language, app.config['OUTPUT_FOLDER'])
+    return render_template('index.html')
 
-        return render_template('index.html', processing_done=True, video_path=output_video_path)
+@app.route('/download/<filename>', methods=['GET'])
+def download(filename):
+    return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run()
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+    app.run(debug=True)
